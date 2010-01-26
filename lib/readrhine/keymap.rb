4abd878cbc4ecc96f48839f124ebf773d7b841cc
emasaka <emasaka@gmail.com>
2010-01-26 11:51:51 +0900

    changed return value of lookup_key_hash.
    Hash -> struct.

--- lib/readrhine/keymap.rb
+++ lib/readrhine/keymap.rb
@@ -2,14 +2,16 @@
 
 module ReadRhine
   class Keymap
+    LookupData = Struct.new('LookupData', :table, :key)
+
     def initialize
       @keymap = {}
     end
 
     def [](key)
       r = lookup_key_hash(key)
-      if r[:key].size == 1
-        r[:hash][r[:key]] or :insert_char
+      if r.key.size == 1
+        r.table[r.key] or :insert_char
       else                      # just to be sure
         raise "#{key.inspect} not found"
       end
@@ -17,13 +19,13 @@ module ReadRhine
 
     def []=(key, fun)
       r = lookup_key_hash(key)
-      if r[:key].size == 1
-        r[:hash][r[:key]] = fun
+      if r.key.size == 1
+        r.table[r.key] = fun
       else
-        e = r[:hash][r[:key][0]]
+        e = r.table[r.key[0]]
         unless Keymap === e
           e = Keymap.new
-          r[:hash][r[:key][0]] = e
+          r.table[r.key[0]] = e
         end
         e[key[1..-1]] = fun
       end
@@ -31,23 +33,23 @@ module ReadRhine
 
     def lookup_keyseq(key)
       r = lookup_key_hash(key)
-      r[:hash][r[:key][0]]
+      r.table[r.key[0]]
     end
 
     protected
 
     def lookup_key_hash(key)
       if key.size == 1
-        { hash: @keymap, key: key }
+        LookupData.new(@keymap, key)
       else
         v = @keymap[key[0]]
         if Keymap === v
           v.lookup_key_hash(key[1..-1])
         else
-          { hash: @keymap, key: key }
+          LookupData.new(@keymap, key)
         end
       end
     end
-  end
 
+  end
 end
