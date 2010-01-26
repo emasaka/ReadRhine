f8420ca11a3a42a91432780290cd2fd47e5da99a
emasaka <emasaka@gmail.com>
2010-01-26 11:17:37 +0900

    added keymap_spec.rb.

---
+++ spec/keymap_spec.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/../lib/readrhine'
+
+describe ReadRhine::Keymap, "when created" do
+  before do
+    @keymap = ReadRhine::Keymap.new
+  end
+
+  it "should no function" do
+    @keymap.lookup_keyseq("\C-h").should == nil
+    @keymap["\C-h"].should == :insert_char
+  end
+
+  it "should binded to single key" do
+    @keymap["\C-h"] = :backward_delete_char
+
+    @keymap["\C-h"].should == :backward_delete_char
+    @keymap.lookup_keyseq("\C-h").should == :backward_delete_char
+  end
+
+  it "should binded to combination key" do
+    @keymap["\e[D"] = :backward_char
+    @keymap["\e[C"] = :forward_char
+
+    @keymap["\e[D"].should == :backward_char
+    @keymap["\e[C"].should == :forward_char
+  end
+end
